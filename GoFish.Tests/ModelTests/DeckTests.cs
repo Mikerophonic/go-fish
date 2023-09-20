e739fb6f5fb750c8bbf4a175d89b2a66bfa7d995
Mikerophonic <micgcon@gmail.com>
2023-09-19 20:34:14 -0700

    WIP

--- GoFish.Tests/ModelTests/DeckTests.cs
+++ GoFish.Tests/ModelTests/DeckTests.cs
@@ -9,5 +9,40 @@ namespace GoFish.Tests
     public class DeckTests
     {
         [TestMethod]
-        public void
+        public void DeckConstructor_CreatesInstanceOfDeck_Deck()
+        {
+            Deck newDeck = new Deck();
+            Assert.AreEqual(typeof(Deck), newDeck.GetType());
+        }
+
+        [TestMethod]
+        public void GetList_ReturnsAList_List()
+        {
+            Deck newDeck2 = new Deck();
+            
+            int expectedCardCount = 52;
+            int actualCardCount = newDeck2.CardsCount;
+
+            Assert.AreEqual(expectedCardCount, actualCardCount);
+        }
+
+        [TestMethod]
+        public void ShuffleDeck_ReturnsShuffledDeck_Deck()
+        {
+            // Deck newDeck3 = new Deck();
+            // Card aCard = Card("hearts", "5");
+            // Card randomCard = newDeck3.ShuffleDeck;
+            // Assert.AreEqual
+
+            Deck deck = new Deck();
+            List<Card> originalOrder = new List<Card>(deck.cards);
+
+            // Act
+            deck.ShuffleDeck();
+            List<Card> shuffledOrder = deck.cards;
+
+            // Assert
+            CollectionAssert.AreNotEqual(originalOrder, shuffledOrder);
+        }
     }
+}
--- GoFish/Models/Deck.cs
+++ GoFish/Models/Deck.cs
@@ -6,6 +6,39 @@ namespace GoFish.Models
 {
     public class Deck
     {
-        
+        private List<Card> cards = new List<Card> {};
+
+        public Deck()
+        {
+            cards = new List<Card>{};
+            InitializeDeck();
+        }
+
+        private void InitializeDeck()
+        {
+            cards = new List<Card>();
+
+            string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+            string[] ranks = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+
+            foreach (var suit in suits)
+            {
+                foreach (var rank in ranks)
+                {
+                    cards.Add(new Card(suit, rank));
+                }
+            }
+        }
+
+            public int CardsCount
+            {
+                get { return cards.Count; }
+            }
+
+            public void ShuffleDeck()
+            {
+                Random random = new Random();
+                cards = cards.OrderBy(card => random.Next()).ToList();
+            }
+        }
     }
-}
